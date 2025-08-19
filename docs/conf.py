bd15b04aaf50cc67f69a4d79aea4bdaffd45dad9
Jurandy Soares <jurandysoares@users.noreply.github.com>
2025-08-19 15:43:27 -0300

    Update html_theme_options to enable edit and view buttons

--- docs/conf.py
+++ docs/conf.py
@@ -34,11 +34,13 @@ html_static_path = ['_static']
 
 html_title = html_short_title = project
 html_theme_options = {
-    'navigation_with_keys': True,
-    'source_repository': 'https://gitea.mange.ifrn.edu.br/tec60/turma-2024/',
-    'source_branch': 'estudantes',
-    'source_directory': 'docs',
-    'source_edit_link': 'https://gitea.mange.ifrn.edu.br/tec60/turma-2024/_edit/estudantes/docs/{filename}',
-    'source_view_link': 'https://gitea.mange.ifrn.edu.br/tec60/turma-2024/src/estudantes/docs/{filename}',
+    "path_to_docs": "docs",
+    "repository_url": "https://github.com/tec60/tec60ifrn",
+    "repository_branch": "main",
+    "use_edit_page_button": True,
+    "use_source_button": True,
+    "use_issues_button": True,
+    "use_repository_button": True,
+    "use_download_button": True
 }
 
